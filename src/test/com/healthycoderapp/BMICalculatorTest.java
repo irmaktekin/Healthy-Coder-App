5eca5058573da1b339d8725521216d7bb34e3afd
irmaktekin <irmaktekin97@gmail.com>
2024-09-16 20:20:42 +0300

    Assertequals used for finding the worst BMI.

--- src/test/com/healthycoderapp/BMICalculatorTest.java
+++ src/test/com/healthycoderapp/BMICalculatorTest.java
@@ -6,6 +6,9 @@ import org.junit.jupiter.api.function.Executable;
 
 import javax.lang.model.element.ExecutableElement;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class BMICalculatorTest {
@@ -45,6 +48,24 @@ class BMICalculatorTest {
 
         //then
         assertThrows(ArithmeticException.class,executable);
+    }
+
+    @Test
+    void should_ReturnCoderWithWorstBMI_When_CoderListNotEmpty(){
+        //given
+        List<Coder> coders = new ArrayList<>();
+        coders.add(new Coder(1.80,60.0));
+        coders.add(new Coder(1.82,98.0));
+        coders.add(new Coder(1.82,64.7));
+
+        //When
+        Coder coderWorstBMI = BMICalculator.findCoderWithWorstBMI(coders);
+        assertAll(
+                ()->assertEquals(1.82,coderWorstBMI.getHeight()),
+                ()->assertEquals(98.0,coderWorstBMI.getWeight())
+        );
+
+
     }
 
 }
