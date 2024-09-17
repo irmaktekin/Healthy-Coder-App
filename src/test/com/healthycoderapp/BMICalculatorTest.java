b9c46d486bf2eafce6aa5c8a539226e47f15f1c9
irmaktekin <irmaktekin97@gmail.com>
2024-09-17 20:54:36 +0300

    CsvFileSource is used for inserting test values from file.

--- src/test/com/healthycoderapp/BMICalculatorTest.java
+++ src/test/com/healthycoderapp/BMICalculatorTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.function.Executable;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -17,7 +18,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class BMICalculatorTest {
     @ParameterizedTest(name = "weight={0}, height={1}")
-    @CsvSource(value = {"89.0,1.72","95.0,1.75","110.0,1.78"})
+    @CsvFileSource(resources = "/diet-recommended-input-data.csv",numLinesToSkip = 1)
     void should_ReturnTrue_When_DietRecommended(Double coderWeight,Double coderHeight){
         //given
         double weight = coderWeight;
